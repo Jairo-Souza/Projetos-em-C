cf8ee3d4a2c40c294cc7fcbcc9c1377eddbb260d
Jairo-Souza <30608674+Jairo-Souza@users.noreply.github.com>
2017-09-21 20:45:49 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,167 @@
+/* 
+ * File:   main.c
+ * Author: Jairo Souza
+ *
+ * Created on 31 de Agosto de 2017, 20:34
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define DELTA 0.000000001
+
+int main(int argc, char** argv) {
+    
+ /*   int a,b,Maior,Menor,positivo,invalido;
+    
+    printf("\n-------------------------------");
+    printf("\nDescobrir qual numero e maior: ");
+    printf("\n");
+    printf("\nDigite o primeiro número: ");
+    scanf("%d", &a);
+    printf("\nDigite o segundo número: ");
+    scanf("%d", &b);
+    Maior =a;
+    Menor =a;
+    if(b < Menor);
+    Menor =b;
+    if(b > Maior);
+    Maior =b;
+    
+    printf("\nResultado foi: %d",  Maior);
+    
+    positivo = Maior;
+    if(Maior<0)
+    {
+         printf("\nnúmero invalido: %d", invalido);
+    } else
+    {
+       printf("\nquadrado desse número e: %d",  Maior*Maior); 
+    }
+*/
+    
+ /*
+    float a;
+    printf("digite um numero: ");
+    scanf("%f", &a);
+ 
+    if(a<0)
+    {
+        printf("Quadrado desse número e: %.0f", a*a);
+    }
+    else{
+        printf("Raiz desse número e: %.0f", sqrt(a));
+    }
+    */
+    
+ /*
+    float a;
+    
+    printf("digite um número: ");
+    scanf("%f", &a);
+    
+      if(a>0)
+    {
+        printf("\nQuadrado desse número e: %.0f", a*a);
+        printf("\nRaiz desse número e: %.0f", sqrt(a));
+    }  else{
+          printf("numero e invalido");
+           }
+    */
+    
+ /*int a;
+    printf("digite um número: ");
+    scanf("%d", &a);
+    
+      if(a%2==0)
+    {
+        printf("\nnúmero e par: %d ",a);        
+    }  else{
+          printf("numero e impar: %d ",a);
+           }
+    */
+    
+/*    int a,b;
+    
+    printf("por favor informe o valor de A: ");
+    scanf("%d", &a );
+    printf("por favor informe o valor de B: ");
+    scanf("%d", &b );
+   
+    if(a > b){
+        printf("\nA é maior: %d", a);
+        printf("\ndiferença: %d", a-b);
+    }else 
+        if(a<b){
+        printf("\nB é maior: %d", b);
+        printf("\ndiferença: %d", b-a);
+        } if(a=b){
+            printf("eles sõa iguais");
+        } */
+
+/*    
+float a,b;
+
+printf("digite a primeira nota:");
+scanf("%f", &a);
+printf("digite a segunda nota:");
+scanf("%f", &b);
+if(a>0)
+{
+printf("Media do aluno e: %.1f", (a+b)/2);
+printf("Finalizado com success");
+} else{
+   printf("nota invalida "); 
+   printf("por favor repor as notas do aluno novamente");
+} */
+
+/*
+    float a,b,div;
+    printf("informe o salario: ");
+    scanf("%f", &a);
+    div = a/100*20; // divisao da porcetangem
+    
+    b = 600; // parcela do emprestimo
+    
+    
+    if(div>=b){
+    printf("seu emprestimo foi ativado com sucess");
+    } else{
+    printf("nao foi possivel a liberção do emprestimo");
+    }*/
+    
+    /*float a,b,c;
+    
+    printf("digite sua Altura: ");
+    scanf("%f", &a);
+    printf("digite seu sexo: ");
+    scanf("%f", &b);
+    
+    //peso
+    //altura
+    //sexo
+    
+    
+    if(b==1){
+        printf("Masculino");
+    }else{
+        printf("Feminino");
+    }
+  
+    */
+    
+    static int a = 5;
+    printf("%d", a--);
+    if(a)
+    main(){
+        int b = 6;
+        printf("%d", b--);
+        return(0);
+    }
+    
+    
+    
+    
+    return (0);
+}
+
